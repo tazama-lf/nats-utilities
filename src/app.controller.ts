a6e9b9cfdf77444601ca18e6dcf8fd7ed8867560
rtkay123 <rodney.kanjala@sybrin.com>
2024-06-05 11:54:53 +0200

    feat: add tms rest proxy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,61 @@ import { config } from './config';
 import { loggerService } from '.';
 import { jetStreamConsume, jetStreamPublish, onJetStreamMessage } from './services/jetStreamService';
 import { natsServiceSubscribe, natsServicePublish, onMessage } from './services/natsService';
+import axios from 'axios';
+
+export const tms = async (ctx: Context): Promise<unknown> => {
+  const { body } = ctx.request;
+  let responseHttp: any = {};
+  try {
+    const { transaction, endpoint, natsConsumer, functionName } = body as any;
+
+    let returnMessage;
+    let subscription;
+
+    switch (config.startupType) {
+      case 'jetstream':
+        let consumer = await jetStreamConsume(natsConsumer, functionName);
+        returnMessage = onJetStreamMessage(consumer);
+        const httpReponseJetStream = await axios.post(endpoint, transaction);
+        await returnMessage.then((message) => {
+          returnMessage = message;
+        });
+
+        responseHttp.tmsResponse = httpReponseJetStream.data;
+        responseHttp.edResponse = returnMessage;
+        break;
+
+      case 'nats':
+        loggerService.log(`nats communication was triggered`);
+        subscription = await natsServiceSubscribe(natsConsumer, functionName);
+        loggerService.log(`Subscription to ${String(natsConsumer)} was done`);
+        returnMessage = onMessage(subscription.subscription);
+        const httpResponseNATS = await axios.post(endpoint, transaction);
+        loggerService.log(`REST Publish to ${String(endpoint)} was done`);
+        await returnMessage.then((message) => {
+          returnMessage = message;
+        });
+
+        responseHttp.tmsResponse = httpResponseNATS.data;
+        responseHttp.edResponse = returnMessage;
+        break;
+      default:
+        break;
+    }
+
+    const resps = await axios.post(endpoint, transaction);
+    ctx.body = responseHttp;
+    ctx.status = resps.status;
+  } catch (error) {
+    loggerService.log(error as string);
+
+    ctx.status = 500;
+    ctx.body = {
+      error,
+    };
+  }
+  return ctx;
+};
 
 export const natsPublish = async (ctx: Context): Promise<any> => {
   try {
