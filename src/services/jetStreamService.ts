8244b8865e14a4fe9f58760c1068dda5b74e6ab8
rtkay123 <rodney.kanjala@sybrin.com>
2024-06-06 12:32:01 +0200

    fix: lint errors

--- src/services/jetStreamService.ts
+++ src/services/jetStreamService.ts
@@ -3,7 +3,7 @@
 import { AckPolicy, type Consumer, type ConsumerConfig, StringCodec, connect } from 'nats';
 import { config } from '../config';
 
-export const jetStreamPublish = async (message: unknown, producerStreamName: string) => {
+export const jetStreamPublish = async (message: unknown, producerStreamName: string): Promise<void> => {
   const natsConn = await connect({
     servers: config.serverUrl,
   });
@@ -45,10 +45,12 @@ export const jetStreamConsume = async (consumerStreamName: string, functionName:
 };
 
 
-export const onJetStreamMessage = async (consumer: Consumer) => {
+export const onJetStreamMessage = async (consumer: Consumer): Promise<string | undefined> => {
   // create a simple consumer and iterate over messages matching the subscription
   const sub = await consumer.consume({ max_messages: 1 });
 
+  // NOTE: disable next line - stream
+  // eslint-disable-next-line no-unreachable-loop
   for await (const message of sub) {
     console.debug(`${Date.now().toLocaleString()} S:[${message?.seq}] Q:[${message.subject}]: ${message.data.length}`);
     const request = message.json<string>();
